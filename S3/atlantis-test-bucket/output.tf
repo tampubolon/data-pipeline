20fbfea33c230ec07b7513886704f5c93ea5b0ad
martinus.tampubolon <martinus.tampubolon@wartek.belajar.id>
2024-11-21 13:01:19 +0700

    fix tf output config

--- S3/atlantis-test-bucket/output.tf
+++ S3/atlantis-test-bucket/output.tf
@@ -1,7 +1,7 @@
 output "bucket_id" {
-  value = aws_s3_bucket.example.id
+  value = aws_s3_bucket.test.id
 }
 
 output "bucket_arn" {
-  value = aws_s3_bucket.example.arn
+  value = aws_s3_bucket.test.arn
 }
