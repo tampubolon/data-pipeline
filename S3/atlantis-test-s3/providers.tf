682816bcc82c52900dc6eeef49190c3cb7b9a226
martinus.tampubolon <martinus.tampubolon@wartek.belajar.id>
2024-11-23 19:56:08 +0700

    revert provider.tf file

---
+++ S3/atlantis-test-s3/providers.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_version = "~> 1.9.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.35.0"
+    }
+  }
+
+  backend "s3" {
+    bucket  = "terraform-martin"
+    key     = "data-pipeline/s3/test-bucket.tfstate"
+    region  = "ap-southeast-1"
+    encrypt = true
+  }
+}
+
+provider "aws" {
+  region = "ap-southeast-1"
+}
